b8151b0d892451e03db622e195bcf8a3527c1133
KaiiMichii <janjantagoylo@gmail.com>
2024-10-03 14:28:04 +0800

    L01-objects-and-classes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log ("Hello World");
+
+        //Input
+
+        const person1 = {
+            name:"January Elijah D. Tagoylo",
+            age:22,
+            status:"Saiyans Pride"
+}
+
+       //Process 
+
+        //Display the Person Attribute in the Console
+        console.log(" Name:" + person1.name);
+        console.log(" Age:" + person1.age);
+        console.log(" Name:" + person1.status);
+
+        //Output
+
+        /* 
+
+         Name:Xhinsade Riri
+         Age:20
+         Name: Tired but Trying Hard
+        
+        */
